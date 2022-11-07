f30e299984a51b2b455a47e6246b9aaaa1a52aa6
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-11-06 18:53:10 -0800

    Adds functions prototypes for each function in spec_functions and for get_spec_func

--- main.h
+++ main.h
@@ -1,8 +1,21 @@
+#include <stdarg.h>
+#include <unistd.h>
+
 #ifndef main_h
 #define main_h
 
 int _printf(const char *format, ...);
-int _putchar(char c, int *p);
+int _putchar(char c);
+typedef struct spec
+{
+	char spec;
+	int (*f)(va_list list);
+} spec_t;
+int spec_char(va_list list);
+int spec_str(va_list list);
+int spec_int(va_list list);
+int spec_other(va_list list);
 void _putnum(int i, int *p);
+int (*get_spec_func(char c))(va_list);
 
 #endif
